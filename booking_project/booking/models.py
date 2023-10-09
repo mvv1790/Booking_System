0d8a7bd96d162b4ebc664fadd1a85589b8130402
mvv1790 <mvv1790@gmail.com>
2023-10-09 08:41:12 +0000

    Installed django, created Django project app, Updated Django models.

---
+++ booking_project/booking/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models 
+
+class Booking(models.Model):
+    name = models.CharField(max_lenght=1000)
+    date = models.Date()
+    time = models..TimeField()
+    
