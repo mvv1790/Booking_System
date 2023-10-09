be85099ca5b31334b750b4a448146658366f7f11
mvv1790 <mvv1790@gmail.com>
2023-10-09 20:40:32 +0000

    Added js static folder and static.js

---
+++ booking_project/booking/static/script.js
@@ -0,0 +1,17 @@
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch ('/submit_booking/', {
+        method:'POST',
+        body: formData, 
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(JSON.stringify(data));
+    })
+    .catch(error=> {
+        console.error('Error:', error)
+    })
+})
