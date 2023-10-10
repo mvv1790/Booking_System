68b041bd025f6d5b3d29a37ae920f5b12c04b7fb
mvv1790 <mvv1790@gmail.com>
2023-10-10 21:38:55 +0000

    Calendar is now marked red for booked days

--- booking_project/booking_app/urls.py
+++ booking_project/booking_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-
+from . import views
 from .views import book_slot, booking_list, calendar_view, get_booked_slots
 
 urlpatterns = [
     path('book_slot/', book_slot, name='book_slot'),
     path('booking_list/', booking_list, name='booking_list'),
-    path('calendar/', calendar_view, name='calendar'),
+    path('calendar/', views.calendar_view, name='calendar'),
     path('get_booked_slots/', get_booked_slots, name='get_booked_slots'),
     path('', RedirectView.as_view(url='/calendar/'), name='index_redirect'),
+    path('get_booked_slots/', views.get_booked_slots, name='get_booked_slots'),
 ]
--- booking_project/booking_app/views.py
+++ booking_project/booking_app/views.py
@@ -1,7 +1,9 @@
+from datetime import date, timedelta
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .forms import BookingForm
 from .models import Booking
+from booking_app.models import Booking
 
 def booking_list(request):
     """View to display all bookings."""
@@ -20,17 +22,34 @@ def book_slot(request):
     return render(request, 'booking_app/book_slot.html', {'form': form})
 
 def get_booked_slots(request):
-    """View to return all booked slots in JSON format."""
-    booked_slots = Booking.objects.all().values('id', 'title', 'start', 'end')
-    return JsonResponse(list(booked_slots), safe=False)
+    bookings = Booking.objects.all()
+
+    data = [{
+        "title": booking.name,
+        "start": f"{booking.date}T{booking.time}",
+        "end": f"{booking.date}T{(datetime.datetime.combine(datetime.date(1,1,1), booking.time) + datetime.timedelta(hours=1)).time()}"
+    } for booking in bookings]
+
+    return JsonResponse(data, safe=False)
 
 # views.py
 
 def calendar_view(request):
-    booked_days = [...] # fetch this from your database or wherever you store booked days
-    days_range = range(1, 32)  # For October as an example
+    # Let's assume you want to show the current month:
+    today = date.today()
+    start_month = date(today.year, today.month, 1)
+    end_month = date(today.year, today.month + 1, 1) - timedelta(days=1)
+
+    # Generating days_range for the month
+    days_range = [(start_month + timedelta(days=i)).day for i in range((end_month - start_month).days + 1)]
+
+    # Fetch booked days from the database:
+    booked_dates = Booking.objects.filter(date__gte=start_month, date__lte=end_month).values_list('date', flat=True)
+    booked_days = [d.day for d in booked_dates]
+
     context = {
+        'days_range': days_range,
         'booked_days': booked_days,
-        'days_range': days_range
     }
+
     return render(request, 'booking_app/calendar.html', context)
