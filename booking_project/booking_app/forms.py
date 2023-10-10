3caeb549d146b406bad19b0d4e43ff04716af7d0
mvv1790 <mvv1790@gmail.com>
2023-10-10 09:34:34 +0000

    Modified form.py

--- booking_project/booking_app/forms.py
+++ booking_project/booking_app/forms.py
@@ -5,3 +5,8 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['name', 'email', 'phone', 'date', 'time']
+
+    def __init__(self, *args, *kwargs):
+        super().__init__(*args, **kwargs)
+        # Make phone field optional
+        self.fields['phone'].required = False
